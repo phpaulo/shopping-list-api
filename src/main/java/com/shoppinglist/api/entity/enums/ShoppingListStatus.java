5a31b612cb91d0d0585847d1b5bd47cc777a7c94
Paulo Henrique <ph.paulo01@gmail.com>
2025-09-12 17:14:52 -0300

    feat: create enuns, such as AuthProvider, SessionStatus and ShoppingListStatus

---
+++ src/main/java/com/shoppinglist/api/entity/enums/ShoppingListStatus.java
@@ -0,0 +1,8 @@
+package com.shoppinglist.api.entity.enums;
+
+// This enum will definite the status of the shopping list, shuch as PLANNING, IN_PROGRESS and COMPLETED.
+public enum ShoppingListStatus {
+    PLANNING,
+    IN_PROGRESS,
+    COMPLETED
+}
