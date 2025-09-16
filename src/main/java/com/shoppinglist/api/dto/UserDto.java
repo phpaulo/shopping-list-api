7ff39a224ecd91e2ee2a4a1d58ffbfc8bb525071
Paulo Henrique <ph.paulo01@gmail.com>
2025-09-16 16:37:04 -0300

    feat: create class of the dto UserDto

---
+++ src/main/java/com/shoppinglist/api/dto/UserDto.java
@@ -0,0 +1,35 @@
+package com.shoppinglist.api.dto;
+
+public class UserDto {
+
+    private String email;
+    private String nome;
+    private String password;
+
+    public UserDto() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
